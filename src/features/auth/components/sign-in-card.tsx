eac5cc324dd03b0f5471a0a2f1028d5c7ba2ff8a
ddk <dattimio95@gmail.com>
2026-01-16 23:41:06 +0100

    basic login error handling

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import { FaGithub } from "react-icons/fa";
+import { TriangleAlert } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -25,7 +26,21 @@ export function SignInCard({ setState }: SignInCardProps) {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const [pending, setPending] = useState(false);
+
+  const onPasswordSignIn = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setPending(true);
+    signIn("password", { email, password, flow: "signIn" })
+      .catch(() => {
+        setError("Invalid email or password");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
 
   const onProviderSignIn = (value: "github") => {
     setPending(true);
@@ -39,11 +54,17 @@ export function SignInCard({ setState }: SignInCardProps) {
       <CardHeader className="px-0 pt-0">
         <CardTitle className="text-2xl">Login</CardTitle>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardDescription>
         Use your email or another service to continue.
       </CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5" action="">
+        <form onSubmit={onPasswordSignIn} className="space-y-2.5">
           <Input
             disabled={pending}
             value={email}
