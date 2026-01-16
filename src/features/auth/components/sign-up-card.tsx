773a2048d4935a3d91efd4b7d06144904e571b56
ddk <dattimio95@gmail.com>
2026-01-16 23:52:30 +0100

    add convex sign up action

--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -1,4 +1,7 @@
-import { FcGoogle } from "react-icons/fc";
+import { useState } from "react";
+
+import { FaGithub } from "react-icons/fa";
+import { TriangleAlert } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -10,31 +13,66 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
-import { FaGithub } from "react-icons/fa";
+
 import { SignInFlow } from "../types";
-import { useState } from "react";
+import { useAuthActions } from "@convex-dev/auth/react";
 
 interface SignUpCardProps {
   setState: (state: SignInFlow) => void;
 }
 
 export function SignUpCard({ setState }: SignUpCardProps) {
+  const { signIn } = useAuthActions();
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+  const [pending, setPending] = useState(false);
+
+  const onPasswordSignUp = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setError("Passwords do not match");
+      return;
+    }
+
+    setPending(true);
+    signIn("password", { email, password, flow: "signUp" })
+      .catch(() => {
+        setError("Something went wrong");
+      })
+      .finally(() => {
+        setPending(false);
+      });
+  };
+
+  const onProviderSignUp = (value: "github") => {
+    setPending(true);
+    signIn(value).finally(() => {
+      setPending(false);
+    });
+  };
 
   return (
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
         <CardTitle className="text-2xl">Sign Up</CardTitle>
       </CardHeader>
+      {!!error && (
+        <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive mb-6">
+          <TriangleAlert className="size-4" />
+          <p>{error}</p>
+        </div>
+      )}
       <CardDescription>
         Use your email or another service to continue.
       </CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5" action="">
+        <form onSubmit={onPasswordSignUp} className="space-y-2.5">
           <Input
-            disabled={false}
+            disabled={pending}
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder="Email"
@@ -42,7 +80,7 @@ export function SignUpCard({ setState }: SignUpCardProps) {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
@@ -50,35 +88,25 @@ export function SignUpCard({ setState }: SignUpCardProps) {
             required
           />
           <Input
-            disabled={false}
+            disabled={pending}
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Confirm password"
             type="password"
             required
           />
-          <Button type="submit" size="lg" className="w-full" disabled={false}>
+          <Button type="submit" size="lg" className="w-full" disabled={pending}>
             Continue
           </Button>
         </form>
         <Separator />
         <div className="flex flex-col gap-y-2.5">
           <Button
-            onClick={() => {}}
+            onClick={() => onProviderSignUp("github")}
             variant="outline"
             size="lg"
             className="w-full relative"
-            disabled={false}
-          >
-            <FcGoogle className="size-5 absolute top-2.5 left-2.5" />
-            Continue with Google
-          </Button>
-          <Button
-            onClick={() => {}}
-            variant="outline"
-            size="lg"
-            className="w-full relative"
-            disabled={false}
+            disabled={pending}
           >
             <FaGithub className="size-5 absolute top-2.5 left-2.5" />
             Continue with Github
