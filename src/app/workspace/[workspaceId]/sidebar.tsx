3d14bf6a88e4a997312a30d664d1ae9cf2e0c74e
ddk <dattimio95@gmail.com>
2026-01-23 23:37:39 +0100

    add SidebarButton component to sidebar

--- src/app/workspace/[workspaceId]/sidebar.tsx
+++ src/app/workspace/[workspaceId]/sidebar.tsx
@@ -1,10 +1,24 @@
+import { Bell, Home, MessagesSquare, MoreHorizontal } from "lucide-react";
+
 import UserButton from "@/features/auth/components/user-button";
+import SidebarButton from "./sidebar-button";
 import WorkspaceSwitcher from "./workspace-switcher";
+import { use } from "react";
+import { usePathname } from "next/navigation";
 
 function Sidebar() {
+  const pathname = usePathname();
   return (
     <aside className="w-17.5 h-full bg-[#007FFF] flex flex-col gap-y-4 items-center pt-2.25 pb-4">
       <WorkspaceSwitcher />
+      <SidebarButton
+        icon={Home}
+        label="Home"
+        isActive={pathname.includes("/workspace")}
+      />
+      <SidebarButton icon={MessagesSquare} label="Messages" />
+      <SidebarButton icon={Bell} label="Activity" />
+      <SidebarButton icon={MoreHorizontal} label="More" />
       <div className="flex flex-col items-center justify-center gap-y-1 mt-auto">
         <UserButton />
       </div>
