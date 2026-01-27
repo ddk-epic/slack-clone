f06ff0cfadb5b93998b7eb0e71d2c20b3eb81e26
ddk <dattimio95@gmail.com>
2026-01-27 22:29:18 +0100

    add channels functionality with API integration

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -1,4 +1,10 @@
-import { AlertTriangle, Loader, MessageSquareText } from "lucide-react";
+import {
+  AlertTriangle,
+  HashIcon,
+  Loader,
+  MessageSquareText,
+  SendHorizonal,
+} from "lucide-react";
 
 import SidebarItem from "./sidebar-item";
 import WorkspaceHeader from "./workspace-header";
@@ -6,6 +12,7 @@ import WorkspaceHeader from "./workspace-header";
 import { useCurrentMember } from "@/features/members/api/use-current-member";
 import { useGetWorkspace } from "@/features/workspaces/api/use-get-workspace";
 import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useGetChannels } from "@/features/channels/api/use-get-channels";
 
 function WorkspaceSidebar() {
   const workspaceId = useWorkspaceId();
@@ -16,7 +23,11 @@ function WorkspaceSidebar() {
     id: workspaceId,
   });
 
-  if (memberLoading || workspaceLoading) {
+  const { data: channels, isLoading: channelsLoading } = useGetChannels({
+    workspaceId,
+  });
+
+  if (memberLoading || workspaceLoading || channelsLoading) {
     return (
       <div className="h-full items-center justify-centerflex flex-col bg-[#5DADFD]">
         <Loader className="size-5 animate-spin text-white" />
@@ -41,6 +52,15 @@ function WorkspaceSidebar() {
       />
       <div className="flex flex-col px-2 mt-3">
         <SidebarItem id="threads" label="Threads" icon={MessageSquareText} />
+        <SidebarItem id="drafts" label="Drafts & Send" icon={SendHorizonal} />
+        {channels?.map((item) => (
+          <SidebarItem
+            key={item._id}
+            id={item._id}
+            label={item.name}
+            icon={HashIcon}
+          />
+        ))}
       </div>
     </div>
   );
