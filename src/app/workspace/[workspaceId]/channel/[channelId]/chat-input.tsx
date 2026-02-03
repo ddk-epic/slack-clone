3a6770826bc1627e5f01e89317a59aad4c304a89
ddk <dattimio95@gmail.com>
2026-02-03 20:18:32 +0100

    feat: implement message creation with api/hooks

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -1,8 +1,13 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 import dynamic from "next/dynamic";
 
 import Quill from "quill";
+import { toast } from "sonner";
+
+import { useCreateMessage } from "@/features/messages/api/use-create-message";
+import { useWorkspaceId } from "@/hooks/use-workspace-id";
+import { useChannelId } from "@/hooks/use-channel-id";
 
 const Editor = dynamic(() => import("@/components/editor"), { ssr: false });
 
@@ -13,24 +18,50 @@ interface ChatInputProps {
 function ChatInput({ placeholder }: ChatInputProps) {
   const editorRef = useRef<Quill | null>(null);
 
-  const handleSubmit = ({
+  const workspaceId = useWorkspaceId();
+  const channelId = useChannelId();
+
+  const [editorKey, setEditorKey] = useState(0);
+  const [isPending, setIsPending] = useState(false);
+
+  const { mutate: createMessage } = useCreateMessage();
+
+  const handleSubmit = async ({
     body,
     image,
   }: {
     body: string;
     image: File | null;
   }) => {
-    console.log({ body, image });
+    try {
+      setIsPending(true);
+      createMessage(
+        {
+          workspaceId,
+          channelId,
+          body,
+        },
+        { throwError: true },
+      );
+
+      // hack: force re-render to clear editor
+      setEditorKey((prevKey) => prevKey + 1);
+    } catch (error) {
+      toast.error("Failed to send message");
+    } finally {
+      setIsPending(false);
+    }
   };
 
   return (
     <div className="w-full px-5">
       <Editor
+        key={editorKey}
         onSubmit={handleSubmit}
         variant="create"
         innerRef={editorRef}
         placeholder={placeholder}
-        disabled={false}
+        disabled={isPending}
       />
     </div>
   );
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -167,7 +167,12 @@ function Editor({
         className="hidden"
       ></input>
       <div className="flex flex-col">
-        <div className="flex flex-col bg-white border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition">
+        <div
+          className={cn(
+            "flex flex-col bg-white border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition",
+            disabled && "opacity-50",
+          )}
+        >
           <div ref={containerRef} className="h-full ql-custom" />
           {image && (
             <div className="p-2">
